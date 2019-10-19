7e9ebbebcb474ee370c69ad90e6152e2b97e3c68
Ashwath Murali <ash.narayan13@gmail.com>
2019-10-19 10:40:36 +0200

    cleaning data

--- json_writer.py
+++ json_writer.py
@@ -1,13 +1,16 @@
 import json
 import pandas
+import re
+import fractions
 
-#df = pandas.read_json('rewrite.json')
 
-#print(df.dtypes)
-#print(df.head())
+# df = pandas.read_json('rewrite.json')
+
+# print(df.dtypes)
+# print(df.head())
 
 def cleanInput(currentDict):
-    #print(currentDict)
+    # print(currentDict)
     new_dict = dict()
     new_dict['name'] = currentDict['name']
     new_dict['date'] = currentDict['date']
@@ -25,16 +28,30 @@ def cleanInput(currentDict):
                 break
         ing[index].pop(0)
         qty[index].pop(0)
-        if len(ing[index])==0:
+        if len(ing[index]) == 0:
             ing[index].append('unknown')
             qty[index].append('0')
         new_dict.setdefault('ingredient', []).append(ing[index])
         new_dict.setdefault('quantity', []).append(qty[index])
         new_dict.setdefault('serves', []).append(serves[index])
         new_dict.setdefault('difficulty', []).append(diff[index])
+    qty_list = new_dict['quantity']
+    ing_list = new_dict['ingredient']
+    for rec in qty_list:
+        if type(rec[0]) is list:
+            # stitch the
+            for i in range(len(rec)):
+                rec[i] = ''.join(rec[i]).lower()
+                rec[i] = re.sub(r"[^a-zA-Z0-9-\--\\]", " ", rec[i])
+    for ing in ing_list:
+        for i in range(len(ing)):
+            ing[i] = ing[i].strip('\n').lower()
     return new_dict
 
+
 def fixEmpty(input):
+    words = ['from', 'optional', 'sized', 'medium', '\\', 'to taste']
+    #normalize = ['cup', 'cups', 'scoop', 'scoops']
     repeat = dict()
     name = input['name']
     time = input['time']
@@ -46,10 +63,31 @@ def fixEmpty(input):
     diff = input['difficulty']
     print(len(name))
     count = 0
-    for i in range(len(ing)):
-        if ing[i][0] == 'unknown':
-            count += 1
-    print(count)
+    # remove unnecessary words
+    for i in range(len(qty)):
+        for val in range(len(qty[i])):
+            for j in words:
+                if j in qty[i][val]:
+                    if j == 'to taste':
+                        qty[i][val] = qty[i][val].replace(j, '1 tsp')
+                    else:
+                        qty[i][val] = qty[i][val].replace(j, '')
+    # get fractions
+    for i in range(len(qty)):
+        for val in range(len(qty[i])):
+            if 'stick' in qty[i][val]:
+                qty[i][val] = qty[i][val][0]
+                #print(float(qty[i][val].split('/')[0])/float(qty[i][val].split('/')[1]))
+    repeat['name'] = name
+    repeat['time'] = time
+    repeat['date'] = date
+    repeat['ingredient'] = ing
+    repeat['serves'] = serves
+    repeat['category'] = cat
+    repeat['difficulty'] = diff
+    repeat['quantity'] = qty
+    return repeat
+
 
 def main():
     with open('rewrite.json', 'r') as f:
@@ -59,6 +97,10 @@ def main():
         json.dump(new_dict, fp, indent=4)
 
     final = fixEmpty(new_dict)
+    with open('clean.json', 'w') as fp:
+        json.dump(final, fp, indent=4)
+
+
 
 if __name__ == '__main__':
     main()
